860182db6b2210f247a98bb2af5155236cd32a11
kateryna_priadchenko <159587758+Ka-teryna@users.noreply.github.com>
2025-08-04 22:55:38 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+nameInput.addEventListener('input', (event) => {
+  const inputValue = event.currentTarget.value.trim();
+  if (inputValue === '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = inputValue;
+  }
+});
